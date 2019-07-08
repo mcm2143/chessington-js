8e96595a9fac4bff1c292dfa6088d1e49af85851
Matt Moore <m-c-moore@outlook.com>
2019-07-08 13:54:33 +0100

    Move pawns up / down 1

--- src/engine/pieces/pawn.js
+++ src/engine/pieces/pawn.js
@@ -1,4 +1,6 @@
 import Piece from './piece';
+import Square from '../square';
+import Player from '../player';
 
 export default class Pawn extends Piece {
     constructor(player) {
@@ -6,6 +8,20 @@ export default class Pawn extends Piece {
     }
 
     getAvailableMoves(board) {
-        return new Array(0);
+        //const boardArray
+        const initialPosition = board.findPiece(this);
+        const availableMoves  = [];
+
+        let availableRow = initialPosition.row
+        let availableCol = initialPosition.col
+
+        if (this.player === Player.WHITE) {
+            availableRow += 1;
+        } else {
+            availableRow -= 1;
+        }
+
+        availableMoves.push(new Square(availableRow, availableCol))
+        return availableMoves;
     }
 }
