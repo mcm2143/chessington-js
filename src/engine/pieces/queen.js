fbe540b1bfa32e138dacf68611622dc3b42d0dae
Matt Moore <m-c-moore@outlook.com>
2019-07-08 14:26:30 +0100

    Move queen

--- src/engine/pieces/queen.js
+++ src/engine/pieces/queen.js
@@ -1,4 +1,5 @@
 import Piece from './piece';
+import Square from '../square';
 
 export default class Queen extends Piece {
     constructor(player) {
@@ -6,6 +7,29 @@ export default class Queen extends Piece {
     }
 
     getAvailableMoves(board) {
-        return new Array(0);
+        const initialPosition = board.findPiece(this);
+        const possibleMoves  = [];
+
+        let currentRow = initialPosition.row
+        let currentCol = initialPosition.col
+
+        for (let i = 0; i < 8; i++) {
+            possibleMoves.push(new Square(i, currentCol))
+            possibleMoves.push(new Square(currentRow, i))
+
+            possibleMoves.push(new Square(currentRow +i, currentCol +i));
+            possibleMoves.push(new Square(currentRow -i, currentCol +i));
+            possibleMoves.push(new Square(currentRow +i, currentCol -i));
+            possibleMoves.push(new Square(currentRow -i, currentCol -i));
+        }
+    
+        const availableMoves = possibleMoves.filter(square => (square.row < 8 
+                                                            && square.row >=0
+                                                            && square.col < 8 
+                                                            && square.col >=0
+                                                            && (square.row != currentRow
+                                                                || square.col != currentCol)))
+
+        return availableMoves;
     }
 }
