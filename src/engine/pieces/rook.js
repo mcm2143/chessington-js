42599477cb295c3717c471ac020817da66447f51
Matt Moore <m-c-moore@outlook.com>
2019-07-08 15:57:21 +0100

    START can't move blocked rook

--- src/engine/pieces/rook.js
+++ src/engine/pieces/rook.js
@@ -13,10 +13,26 @@ export default class Rook extends Piece {
         let currentRow = initialPosition.row
         let currentCol = initialPosition.col
 
-        for (let i = 0; i < board.board.length; i++) {
-            possibleMoves.push(new Square(i, currentCol))
-            possibleMoves.push(new Square(currentRow, i))
+        const directionVectors = [{row:0,  col:1},
+                                  {row:0,  col:-1},
+                                  {row:1,  col:0},
+                                  {row:-1, col:0}];
+        
+        for (const direction of directionVectors) {
+            let currentSquare = initialPosition;
+            
+            for (let i = 0; i < board.board.length; i++) {
+                currentSquare = currentSquare.nextSquare(direction);
+                console.log(board.getPiece(currentSquare));
+                
+                if (board.getPiece(currentSquare) !== undefined) {
+                    break;
+                } else {
+                    possibleMoves.push(currentSquare);
+                }
+            }
         }
+
         const availableMoves = board.removeInvalidMoves(initialPosition, possibleMoves);
         return availableMoves;
     }
--- src/engine/square.js
+++ src/engine/square.js
@@ -12,6 +12,12 @@ export default class Square {
         return !!otherSquare && this.row === otherSquare.row && this.col === otherSquare.col;
     }
 
+    nextSquare (direction) {
+        const nextRow = this.row + direction.row;
+        const nextCol = this.col + direction.col;
+        return new Square(nextRow, nextCol);
+    }
+
     toString() {
         return `Row ${this.row}, Col ${this.col}`;
     }
