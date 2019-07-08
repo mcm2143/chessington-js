4a9c4a2f72deeb96da828d654f185c1755ede092
Matt Moore <m-c-moore@outlook.com>
2019-07-08 14:16:11 +0100

    Move bishops

--- src/engine/pieces/bishop.js
+++ src/engine/pieces/bishop.js
@@ -1,4 +1,5 @@
 import Piece from './piece';
+import Square from '../square';
 
 export default class Bishop extends Piece {
     constructor(player) {
@@ -6,6 +7,24 @@ export default class Bishop extends Piece {
     }
 
     getAvailableMoves(board) {
-        return new Array(0);
+        const initialPosition = board.findPiece(this);
+        const possibleMoves  = [];
+
+        let currentRow = initialPosition.row
+        let currentCol = initialPosition.col
+
+        for (let i = 1; i < 8; i++) {
+            possibleMoves.push(new Square(currentRow +i, currentCol +i));
+            possibleMoves.push(new Square(currentRow -i, currentCol +i));
+            possibleMoves.push(new Square(currentRow +i, currentCol -i));
+            possibleMoves.push(new Square(currentRow -i, currentCol -i));
+        }
+
+        const availableMoves = possibleMoves.filter(square => (square.row < 8 
+                                                            && square.row >=0
+                                                            && square.col < 8 
+                                                            && square.col >=0))
+        
+        return availableMoves;
     }
 }
